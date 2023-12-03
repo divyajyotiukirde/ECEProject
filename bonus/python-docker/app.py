0d5ff95165f50be0b930b9227601529fbe4376a6
Divyajyoti Ukirde <30872426+divyajyotiuk@users.noreply.github.com>
2023-12-03 15:42:44 -0500

    cpu str

--- bonus/python-docker/app.py
+++ bonus/python-docker/app.py
@@ -12,6 +12,7 @@ def hello_world():
 def submit_job():
     job_str = request.args.get('job')
     cpu_str = request.args.get('cpu')
+    job_scheduler.update_cpu(cpu_str)
     job_scheduler.add_in_queue(job_str)
     job_scheduler.process_queue()
     return job_str + ' Job submitted!'
@@ -21,5 +22,5 @@ def stop_job():
     return ' Job stopped!'
 
 # comment this when building docker image
-# if __name__ == '__main__':
-#     app.run(debug=True)
+if __name__ == '__main__':
+    app.run(debug=True)
--- bonus/python-docker/script.sh
+++ bonus/python-docker/script.sh
@@ -1,5 +1,3 @@
 #!/bin/sh
 
-kubectl proxy & 
-
 python3 -m flask run --host=0.0.0.0
