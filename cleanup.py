f62406add622a61affddfc903d7eaa30fbed1c00
Divyajyoti Ukirde <30872426+divyajyotiuk@users.noreply.github.com>
2023-12-06 15:18:26 -0500

    cleanup changes

--- cleanup.py
+++ cleanup.py
@@ -10,17 +10,20 @@ def clean_pods():
     command = f"kubectl delete pod --field-selector=status.phase==Succeeded"
     result = subprocess.run(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
     print(result.stdout)
+    command = f"rm stress-pod-*.yaml"
+    result = subprocess.run(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+
+
+def enable_nodes():
     command = f"kubectl uncordon node1.group5project.ufl-eel6871-fa23-pg0.utah.cloudlab.us"
     result = subprocess.run(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
     print(result.stdout)
     command = f"kubectl uncordon node2.group5project.ufl-eel6871-fa23-pg0.utah.cloudlab.us"
     result = subprocess.run(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
     print(result.stdout)
-    command = f"rm stress-pod-*.yaml"
-    result = subprocess.run(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
 
 
 if __name__ == '__main__':
     while True:
         clean_pods()
-        time.sleep(300)
+        enable_nodes()
